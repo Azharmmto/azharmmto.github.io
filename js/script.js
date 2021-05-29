5c7fbcb5fd4370198300fc54c582a23bc56d240a
Azharmmto <azharmamonto7@gmail.com>
2021-05-29 14:23:07 +0800

    Menambahkan style pada gambar fawaid

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 // todo : navbar
 const nav = document.querySelector("nav"),
-  menuHamburger = document.querySelector("nav .menu-hamburger");
+   menuHamburger = document.querySelector("nav .menu-hamburger"),
+   body = document.querySelector("body");
+
+// font body jika tdk terhubung ke jaringan
+if ('body.style.fontFamily = "sans-serif" ') {
+   body.style.fontFamily = "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Open Sans', 'Helvetica Neue'";
+} else {
+   body.style.fontFamily = "sans-serif";
+}
 
 menuHamburger.onclick = () => {
-  menuHamburger.classList.toggle("close");
-  nav.classList.toggle("mobile");
+   menuHamburger.classList.toggle("close");
+   nav.classList.toggle("mobile");
 };
